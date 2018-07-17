69af2d84389c2fc841755e45f220106eb614771a
linshengwen <momojy@vip.qq.com>
2018-07-17 18:06:14 +0800

    init 补充 circle

--- src/main/java/cn/momosv/blog/ws/controller/WSController.java
+++ src/main/java/cn/momosv/blog/ws/controller/WSController.java
@@ -2,7 +2,6 @@ package cn.momosv.blog.ws.controller;
 
 import cn.momosv.blog.ws.model.TbUserPO;
 
-import cn.momosv.blog.ws.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +11,5 @@ import java.util.UUID;
 @RestController
 public class WSController {
 
-    @Autowired
-    UserService userService;
 
-    @RequestMapping("/addUser")
-    public String addUser(){
-        TbUserPO userPO = new TbUserPO();
-        userPO.setId(UUID.randomUUID().toString());
-        userPO.setUserName("momo");
-        userService.insertOne(userPO);
-        TbUserPO userPO1= userService.selectByPrimaryKey1(userPO.getId());
-        return userPO1.toString();
-    }
 }
